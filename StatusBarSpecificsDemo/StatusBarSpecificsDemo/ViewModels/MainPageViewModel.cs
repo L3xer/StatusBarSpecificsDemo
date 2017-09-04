d736a1e6e46cbf43c84bfeb8d33b0aafa4997a70
Sanya <sanya2371@gmail.com>
2017-09-04 20:35:56 +0300

    Add project files.

--- StatusBarSpecificsDemo/StatusBarSpecificsDemo/ViewModels/MainPageViewModel.cs
+++ StatusBarSpecificsDemo/StatusBarSpecificsDemo/ViewModels/MainPageViewModel.cs
@@ -1,40 +1,10 @@
-﻿using Prism.Commands;
-using Prism.Mvvm;
+﻿using Prism.Mvvm;
 using Prism.Navigation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace StatusBarSpecificsDemo.ViewModels
 {
-    public class MainPageViewModel : BindableBase, INavigationAware
+    public class MainPageViewModel : BindableBase
     {
-        private string _title;
-        public string Title
-        {
-            get { return _title; }
-            set { SetProperty(ref _title, value); }
-        }
-
-        public MainPageViewModel()
-        {
-
-        }
-
-        public void OnNavigatedFrom(NavigationParameters parameters)
-        {
-
-        }
-
-        public void OnNavigatingTo(NavigationParameters parameters)
-        {
-
-        }
-
-        public void OnNavigatedTo(NavigationParameters parameters)
-        {
-            if (parameters.ContainsKey("title"))
-                Title = (string)parameters["title"] + " and Prism";
-        }
+       
     }
 }
